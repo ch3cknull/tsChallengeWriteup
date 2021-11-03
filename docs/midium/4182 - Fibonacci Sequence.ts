bdf4a1759f9f34253beb99a65362937cb861919c
ch3cknull <ch3cknull@outlook.com>
2021-11-03 15:09:30 +0800

    refactor: 4182

--- docs/midium/4182 - Fibonacci Sequence.ts
+++ docs/midium/4182 - Fibonacci Sequence.ts
@@ -1,19 +1,15 @@
-type originNumber = 1 | 2
-
-type getLength<T extends readonly any[]> = T['length']
-
 type ArrayFill<C extends number, R extends any[] = []> =
-`${C}` extends `0` | `-${number}` 
+`${C}` extends `-${number}` 
   ? []
   : R['length'] extends C ? R : ArrayFill<C, [...R, 0]>
 
 // only fit positive number :(
 // since fibonacci start from 1
-type subOne<T extends number> = ArrayFill<T> extends [infer _, ...infer U] ? getLength<U> : never
+type subOne<T extends number> = ArrayFill<T> extends [infer _, ...infer U] ? U['length'] : never
 
-type Add<a extends number, b extends number> = getLength<[...ArrayFill<a>, ...ArrayFill<b>]>
+type Add<a extends number, b extends number> = [...ArrayFill<a>, ...ArrayFill<b>]['length']
 
 type Fibonacci<T extends number> =
-T extends originNumber 
+T extends 1 | 2
   ? 1
   : Add<Fibonacci<subOne<T>>, Fibonacci<subOne<subOne<T>>>>
