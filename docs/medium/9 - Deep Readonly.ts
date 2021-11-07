58823b418ee4630de5c9367156982a093b8d660c
ch3cknull <ch3cknull@outlook.com>
2021-11-07 16:12:58 +0800

    update 9

--- docs/medium/9 - Deep Readonly.ts
+++ docs/medium/9 - Deep Readonly.ts
@@ -3,3 +3,12 @@ type DeepReadonly<T> = keyof T extends never
   : {
       readonly [P in keyof T]: DeepReadonly<T[P]>
     }
+
+
+    
+type isPureObjet<T> = T extends object ? T extends Function ? false : true: false
+
+type DeepReadonly2<T> = {
+  readonly [P in keyof T] : 
+    isPureObjet<T[P]> extends true ? DeepReadonly<T[P]> : T[P]
+}
