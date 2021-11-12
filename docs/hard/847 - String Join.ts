0c765d63124df14fe66e46666cf2a17ab5f5a277
ch3cknull <ch3cknull@outlook.com>
2021-11-12 13:28:23 +0800

    update 847

---
+++ docs/hard/847 - String Join.ts
@@ -0,0 +1,8 @@
+type Slice<T extends string> = T extends `${infer _}${infer R}` ? R : T 
+
+type JoinType<T extends any[], JoinSymbol extends string, Result extends string = ''> =
+T extends [infer L, ...infer R]
+  ? JoinType<R, JoinSymbol, `${Result}${JoinSymbol}${L & string}`>
+  : JoinSymbol extends '' ? Result : Slice<Result>
+
+declare function join<T extends string>(delimiter: T): <R extends string[]>(...parts: R) => JoinType<R, T>;
